3b94e632551c9f5a5a825a81fcda035af9ec5e9c
wb1110 <strengthcoachwb@gmail.com>
2022-01-18 08:21:09 -0600

    checkbyid

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -2,12 +2,15 @@ exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
+  next()
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
+  next()
 }
 
 exports.checkAccountId = (req, res, next) => {
   // DO YOUR MAGIC
+  next()
 }
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,33 +1,44 @@
 const router = require('express').Router()
+const md = require('./accounts-middleware')
+const Account = require('./accounts-model')
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/', async (req, res, next) => {
   try {
-    res.json([{}, {}])
+    const accounts = await Account.getAll()
+    res.json(accounts)
   } catch (err) {
     next(err)
   }
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', md.checkAccountId, async (req, res, next) => {
   try {
-
+    const account = await Account.getById(req.params.id)
+    res.json(account)
   } catch (err) {
     next(err)
   }
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-  try {
+router.post(
+  '/',
+  md.checkAccountPayload,
+  md.checkAccountNameUnique,
+  (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
 
-  } catch (err) {
-    next(err)
-  }
+    } catch (err) {
+      next(err)
+    }
 })
 
-router.put('/:id', (req, res, next) => {
+router.put(
+  '/:id',
+  md.checkAccountId,
+  md.checkAccountPayload,
+  md.checkAccountNameUnique,
+  (req, res, next) => {
   // DO YOUR MAGIC
   try {
 
